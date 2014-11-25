aaa9db6859d8154ea27977982b24bd5f3a72319b
thelowdowndev <thelowdowndev@gmail.com>
2014-11-25 17:54:21 -0500

    Fixed Configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,14 +27,14 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.applications.secrets.email_provider_username,
-    password: Rails.application.sectrys.email_provider_password
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
   }
 
   #ActionMailer Config
