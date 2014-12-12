5f4300526177b091c0f5f50269b963a8fdcb1ecc
thelowdowndev <thelowdowndev@gmail.com>
2014-12-11 22:08:31 -0500

    Added mailing system

---
+++ app/mailers/usermailer.rb
@@ -0,0 +1,9 @@
+class Usermailer < ActionMailer::Base
+  default from: "do-not-reply@example.com"
+
+  def contact_email(contact)
+    @contact = contact
+    mail(to: Rails.application.secrets.owner_email,
+    from: @contact.email, :subject => "Website Contact")
+  end
+end
